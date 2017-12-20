d6df06678d5de0695b5750f5aa47f9c02128f031
Babu Thomas <babuthomas138@gmail.com>
2017-12-20 16:14:31 +0530

    Add another solution for array rotate problem

--- array_rotate.cpp
+++ array_rotate.cpp
@@ -4,7 +4,8 @@
 // Input -  1, 2, 3, 4, 5 and r = 2
 // Output - 4, 5, 1, 2, 3
 
-// 1. Solution using temp array. Time - O(N), Space - O(r)
+// 1. Solution using temp array. Time - O(N), Space - O(r).
+// 2. Solution in which "r" one element rotations are performed. Time - O(N * r), Space - O(1).
 
 #include <iostream>
 #include <vector>
@@ -24,10 +25,30 @@ void rotate1(vector<int>& a, int r)
 		a[i] = temp[i];
 }
 
+void rotate2(vector<int>& a, int r)
+{
+	int n = a.size();
+	r = r % n;
+	for(int i = 1; i <= r; i++)
+	{
+		// Save last element
+		int save = a[n - 1];
+		// Shift other elements by 1
+		for(int j = n - 2; j >= 0; j--)
+			a[j + 1] = a[j];
+		// Put last element in its right place
+		a[0] = save;
+	}
+}
+
 int main()
 {
 	vector<int> a = {1, 2, 3, 4, 5, 6};
 	rotate1(a, 10);
+	for(auto &i: a)
+		cout << i << " ";
+	cout << "\n";
+	rotate2(a, 10)
 	for(auto &i: a)
 		cout << i << " ";
 	cout << "\n";
