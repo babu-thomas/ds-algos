ee1f6e6d82be12f69594f2b0d177c81ca67cc0d6
Babu Thomas <babuthomas138@gmail.com>
2017-12-30 13:09:26 +0530

    Add constant space solution for max min array problem

--- max_min_array.cpp
+++ max_min_array.cpp
@@ -7,6 +7,7 @@
 // Output - 5, 1, 4, 2, 3
 
 // 1. Solution using two pointers and extra space. Time - O(N), Space - O(N).
+// 2. Solution using base changing to store 2 numbers at one place. Time - O(N), Space - O(1).
 
 #include <iostream>
 #include <vector>
@@ -32,6 +33,25 @@ vector<int> max_min1(const vector<int>& a)
 	return result;
 }
 
+void max_min2(vector<int>& a)
+{
+	int n = a.size();
+	int base = a[n - 1] + 1;
+	int min_idx = 0, max_idx = n - 1;
+	for(int i = 0; i < n; i++) {
+		if(i % 2 == 0) {
+			a[i] = (a[max_idx] % base) * base + a[i];
+			max_idx--;
+		}
+		else {
+			a[i] = (a[min_idx] % base) * base + a[i];
+			min_idx++;
+		}
+	}
+	for(int i = 0; i < n; i++)
+		a[i] = a[i] / base;
+}
+
 void print(const vector<int>& a)
 {
 	for(auto &i: a)
@@ -43,9 +63,15 @@ int main()
 {
 	vector<int> a1 = {1, 2, 3, 4, 5, 6, 7};
 	vector<int> a2 = {1, 2, 3, 4, 5, 6};
+
 	print(a1);
 	print(max_min1(a1));
+	max_min2(a1);
+	print(a1);
 	print(a2);
 	print(max_min1(a2));
+	max_min2(a2);
+	print(a2);
+
 	return 0;
 }
