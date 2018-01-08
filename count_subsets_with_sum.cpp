d8b685ab0bedd727762bcb355351a437f99ece86
Babu Thomas <babuthomas138@gmail.com>
2018-01-09 01:08:43 +0530

    Add DP solution for count subsets with given sum problem

--- count_subsets_with_sum.cpp
+++ count_subsets_with_sum.cpp
@@ -5,6 +5,7 @@
 // Output - 2
 
 // 1. Recursive solution. For every element, include or exclude it in the subset. Exponential time.
+// 2. DP solution. Time - O(N * k), Space - O(N * k).
 
 #include <iostream>
 #include <vector>
@@ -12,11 +13,13 @@ using namespace std;
 
 int count_subsets1(const vector<int>& a, int sum);
 int rec(const vector<int>& a, int sum, int index);
+int count_subsets2(const vector<int>& a, int sum);
 
 int main()
 {
 	vector<int> a = {1, 2, 3, 4, 5};
 	cout << count_subsets1(a, 6) << "\n";
+	cout << count_subsets2(a, 6) << "\n";
 	return 0;
 }
 
@@ -36,4 +39,34 @@ int rec(const vector<int>& a, int sum, int index)
 		return rec(a, sum, index - 1);
 	
 	return rec(a, sum, index - 1) + rec(a, sum - a[index], index - 1);
+}
+
+int count_subsets2(const vector<int>& a, int sum)
+{
+	int n = a.size();
+	// dp[i][j] = number of subsets upto index i that sum to j.
+	int dp[n][sum + 1];
+	
+	// Number of subsets with sum 0 is 1
+	for(int i = 0; i < n; i++)
+		dp[i][0] = 1;
+		
+	// If only first element present in set
+	for(int j = 1; j <= sum; j++) {
+		if(a[0] == j)
+			dp[0][j] = 1;
+		else
+			dp[0][j] = 0;
+	}
+	
+	for(int i = 1; i < n; i++) {
+		for(int j = 1; j <= sum; j++) {
+			if(a[i] > j)
+				dp[i][j] = dp[i - 1][j];
+			else
+				dp[i][j] = dp[i - 1][j] + dp[i - 1][j - a[i]];
+		}
+	}
+	
+	return dp[n - 1][sum];
 }
