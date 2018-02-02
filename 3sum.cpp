3306fa467dd115a6238b57b664589c16aeaf090a
Babu Thomas <babuthomas138@gmail.com>
2018-02-03 00:46:47 +0530

    Add solution using sorting then 2 pointer approach for 3sum problem

--- 3sum.cpp
+++ 3sum.cpp
@@ -8,10 +8,12 @@
 
 // 1. Naive solution. Try all possible triplets. Time - O(N^3), Space - O(1).
 // 2. Use Hash Table. Time - O(N^2), Space - O(N).
+// 3. Sort the array then use two pointer approach. Time - O(N^2), Space - O(1).
 
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 void three_sum1(vector<int> a)
@@ -55,11 +57,45 @@ void three_sum2(vector<int> a)
 		cout << "No triplet with zero sum" << "\n";
 }
 
+void three_sum3(vector<int> a)
+{
+	sort(a.begin(), a.end());
+	int size = a.size();
+	bool found = false;
+	for(int i = 0; i < size - 2; i++) {
+		int first = a[i];
+		int left = i + 1;
+		int right = size - 1;
+		while(left < right) {
+			int second = a[left];
+			int third = a[right];
+			int sum = first + second + third;
+			if(sum == 0) {
+				found = true;
+				cout << first << ", " << second << ", " << third << "\n";
+				left++;
+				right--;
+			}
+			else if(sum < 0) {
+				left++;
+			}
+			else {
+				right--;
+			}
+		}
+	}
+	
+	if(!found)
+		cout << "No triplet with zero sum" << "\n";
+}
+
 int main()
 {
 	vector<int> a = {-1, 0, 1, 2, -1, -4};
 	three_sum1(a);
 	cout << "\n";
 	three_sum2(a);
+	cout << "\n";
+	three_sum3(a);
 	return 0;
 }
