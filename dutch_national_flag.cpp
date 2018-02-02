4538f054864f743d850c8fadd2477926b276b584
Babu Thomas <babuthomas138@gmail.com>
2018-02-03 05:18:58 +0530

    Add counting sort solution for dutch national flag problem

---
+++ dutch_national_flag.cpp
@@ -0,0 +1,35 @@
+// Given an array containing only 0’s, 1’s and 2’s, sort the array in linear time and using
+// constant space.
+// For example,
+// Input - 1, 1, 0, 2, 0, 1
+// Output - 0, 0, 1, 1, 1, 2
+
+// 1. Count occurrences of 0, 1 and 2. Then put them in correct order. Time - O(N), Space - O(1).
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void linear_sort1(vector<int>& a)
+{
+	int count[3] = {0};
+	for(int i = 0; i < a.size(); i++) {
+		count[a[i]]++;
+	}
+	
+	int index = 0;
+	for(int i = 0; i < 3; i++) {
+		for(int j = 0; j < count[i]; j++)
+			a[index++] = i;
+	}
+}
+
+int main()
+{
+	vector<int> a = {1, 2, 1, 0, 2, 0, 1};
+	linear_sort1(a);
+	for(auto& i: a)
+		cout << i << " ";
+	cout << "\n";
+	return 0;
+}
