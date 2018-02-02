9cc6c4520999307437d95b502d9faf68d6aa6687
Babu Thomas <babuthomas138@gmail.com>
2018-02-02 08:55:01 +0530

    Add solution using hashing for 2sum problem

--- 2sum.cpp
+++ 2sum.cpp
@@ -4,9 +4,12 @@
 // Output - 0, 2 (Indices of 8 and 2)
 
 // 1. Naive solution. Try all possible pairs. Time - O(N^2), Space - O(1).
+// 2. Use Hash table. Time - O(N), Space - O(N).
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <unordered_map>
 using namespace std;
 
 void two_sum1(vector<int> a, int target)
@@ -27,10 +30,32 @@ void two_sum1(vector<int> a, int target)
 		cout << "No pair with given sum" << "\n";
 }
 
+void two_sum2(vector<int> a, int target)
+{
+	unordered_map<int, int> pairs;
+	bool pair_found = false;
+	for(int i = 0; i < a.size(); i++) {
+		int this_pair = a[i];
+		int other_pair = target - this_pair;
+		if(pairs.count(other_pair) > 0) {
+			pair_found = true;
+			cout << pairs[other_pair] << ", " << i << "\n";
+		}
+		pairs[this_pair] = i;
+	}
+	
+	if(!pair_found)
+		cout << "No pair with given sum" << "\n";
+}
+
 int main()
 {
 	vector<int> a = {8, 7, 2, 5, 3, 1};
-	int target = 10;
-	two_sum1(a, target);
+	int target1 = 10;
+	int target2 = 16;
+	two_sum1(a, target1);
+	two_sum1(a, target2);
+	two_sum2(a, target1);
+	two_sum2(a, target2);
 	return 0;
 }
