f6f8c52e393ce3b08183006df42a7abb25f375f4
Babu Thomas <babuthomas138@gmail.com>
2018-02-06 06:09:06 +0530

    Add sliding window solution for longest non repeating substring problem

--- longest_non_repeating_substring.cpp
+++ longest_non_repeating_substring.cpp
@@ -5,6 +5,7 @@
 
 // 1. Naive solution. Check every substring for uniqueness. Time - O(N^3), Space - O(S). S is the
 // size of the alphabet.
+// 2. Sliding window solution. Time - O(N), Space - O(S). S is the size of the alphabet.
 
 #include <iostream>
 #include <string>
@@ -14,6 +15,7 @@ using namespace std;
 
 int longest_non_repeating_substring1(const string& s);
 bool is_unique(const string& s, int start, int end);
+int longest_non_repeating_substring2(const string& s);
 
 int main()
 {
@@ -23,6 +25,11 @@ int main()
 	cout << longest_non_repeating_substring1(s1) << "\n"
 		 << longest_non_repeating_substring1(s2) << "\n"
 		 << longest_non_repeating_substring1(s3) << "\n";
+		 
+	cout << longest_non_repeating_substring2(s1) << "\n"
+		 << longest_non_repeating_substring2(s2) << "\n"
+		 << longest_non_repeating_substring2(s3) << "\n";
+
 	return 0;
 }
 
@@ -51,4 +58,26 @@ bool is_unique(const string& s, int start, int end)
 	}
 	
 	return true;
+}
+
+int longest_non_repeating_substring2(const string& s)
+{
+	int size = s.size();
+	int i = 0, j = 0, max_len = 0;
+	unordered_map<int, bool> found;
+	while(i < size && j < size) {
+		// If jth character not already present, extend the substring by adding it
+		if(!found[s[j]]) {
+			found[s[j]] = true;
+			max_len = max(max_len, j - i + 1);
+			j++;
+		}
+		else {
+			// Remove ith character or the first character from current substring
+			found[s[i]] = false;
+			i++;
+		}
+	}
+	
+	return max_len;
 }
